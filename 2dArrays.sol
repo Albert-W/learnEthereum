584f9f76f4c6021934c2df91741ec86f2b9cbdc0
Albert <wuyichang1990@gmail.com>
2019-09-09 15:51:39 +0100

    titan v2

--- 2dArrays.sol
+++ 2dArrays.sol
@@ -1,23 +1,56 @@
 pragma solidity ^0.5.0;
-pragma experimental ABIEncoderV2;
+// pragma experimental ABIEncoderV2;
 
 contract twoDArrays {
-    uint[][] public myArrays;
-    uint id =0;
+    uint[][] public myArrays = [
+        [1,2],
+        [3,4]
+    ] ;
+    uint[] public myList=[
+        15,
+        20
+    ];
+    // uint id =0;
 
     function addtoArray(uint[] memory l) public {
         myArrays.push(l);
-        id++;
+        // id++;
     }
     
     function indextoValue(uint i) public view returns (uint[] memory ) {
         return myArrays[i];
     }
 
+    // amplify the matrix to l * l ;
+    function amplify(uint l) public {
+        myArrays.length = l;
+        for(uint a =0;a < l; a++){
+            myArrays[a].length =l;
+        }
+    }
+
+    // change it to one dimensional array 
+    function offloadRep() public view returns(uint[] memory){
+        uint l = myArrays.length;
+        uint[] memory list;
+        list = new uint[](l*l);
+        for(uint i =0;i<l; i++){
+            for(uint j =0;j<l; j++){
+                list[i*l +j] = myArrays[i][j];
+            }
+        }
+        return list;        
+    }
+
+    function setList(uint[] memory newList) public {
+        myList = newList;
+    }
+
     // return uint[][] is only supported in the new experimental ABI encoder.
-    function getArray() public view returns (uint[][] memory){
-        return myArrays;
-    } 
+    // function getArray() public view returns (uint[][] memory){
+    //     return myArrays;
+    // } 
+
 }
 
 // all the functions pass the test. 
--- TITAN.sol
+++ TITAN.sol
@@ -6,19 +6,31 @@ contract Titan {
         'id2',
         'id3'
     ];
-    string[] leaveList;
-    uint[] replist;
-    uint[] eiglist;
-    uint[][] repMatrix;
-    mapping (string => string[]) providerDataList; 
-    mapping (string => string[]) consumerDataList;
-    mapping (string => uint) dataValue;
+    string[] public leaveList;
+    uint[] public repList =[
+        10,
+        20,
+        30
+    ];
+    uint[] public eigList = [
+        13,
+        25,
+        34
+    ];
+    uint[][] public repMatrix= [
+        [1,2,3],
+        [3,4,6],
+        [7,8,9]
+    ];
+    mapping (int => string[]) public providerDataList;
+    mapping (int => string[]) public consumerDataList;
+    mapping (string => uint) public dataValue;
 
     function getlength() public view returns (uint){
         return userList.length;
     }
 
-    function IndexOf(string memory uid) public view returns (int){
+    function indexOf(string memory uid) public view returns (int){
         uint i = 0;
         while(keccak256(bytes(userList[i])) != keccak256(bytes(uid)) ){
             i++;
@@ -30,15 +42,66 @@ contract Titan {
     }
 
     function join(string memory uid) public {
-        if(IndexOf(uid) == -1){
+        uint l = userList.length;
+        if(indexOf(uid) == -1){
             userList.push(uid);
+            repList.length = l+1;
+            eigList.length = l+1;
+            repMatrix.length = l+1;
+        }
+        for(uint i = 0;i<l+1;i++){
+            repMatrix[i].length = l+1;
         }
     }
     function leave(string memory uid) public {
-        if(IndexOf(uid) == -1){
+        if(indexOf(uid) == -1){
             leaveList.push(uid);
         }
     }
+    function getRep(string memory uid) public view returns (uint) {
+        int i = indexOf(uid);
+        if(i == -1) return 0;
+        return repList[uint(i)];
+    }
+    function getEig(string memory uid) public view returns (uint) {
+        int i = indexOf(uid);
+        if(i == -1) return 0;
+        return eigList[uint(i)];
+    }
+    function _setDataValue(string memory k, uint v) public {
+        dataValue[k] = v;
+    }
+    function _setProviderDataList(int i, string memory k) public {
+        providerDataList[i].push(k);
+    }
+    function _setRepMatrix(int i,int j, uint v) public {
+        repMatrix[uint(i)][uint(j)] += v;
+    }
 
 
+    function updataRep(string memory k, uint v, string memory uid1, string memory uid2) public {
+        int i = indexOf(uid1);
+        int j = indexOf(uid2);
+        dataValue[k] = v;
+        providerDataList[i].push(k);
+        consumerDataList[j].push(k);
+        repMatrix[uint(i)][uint(j)] += v;
+        repList[uint(i)] += v;
+    }
+
+    function offloadRep() public view returns(uint[] memory){
+        uint l = repMatrix.length;
+        uint[] memory list;
+        list = new uint[](l*l);
+        for(uint i = 0;i<l; i++){
+            for(uint j = 0;j<l; j++){
+                list[i*l + j] = repMatrix[i][j];
+            }
+        }
+        return list;
+    }
+
+    function update (uint[] memory eigTrustRes) public{
+        eigList = eigTrustRes;
+    }
 }
